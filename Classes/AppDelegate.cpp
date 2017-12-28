c172d26cc8d9363f267635bcc9db51ac680986a1
hly00 <32394742+hly00@users.noreply.github.com>
2017-12-28 20:34:24 +0800

    Add files via upload

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -26,7 +26,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    CCScene *pScene = GameMenuLayer::scene();
+    //CCScene *pScene = GameScene::create();
+	CCScene *pScene=GameMenuLayer::scene();
 
     // run
     pDirector->runWithScene(pScene);
